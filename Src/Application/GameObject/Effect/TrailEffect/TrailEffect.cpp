12e3a5fbf85d0c39ec12b9bc5b1bfb38639a351d
myakuru <sh1016n@icloud.com>
2025-09-08 12:10:56 +0900

    Trail追加

---
+++ Src/Application/GameObject/Effect/TrailEffect/TrailEffect.cpp
@@ -0,0 +1,21 @@
+﻿#include "TrailEffect.h"
+
+void TrailEffect::Init()
+{
+	KdGameObject::Init();
+	m_trailPolygon = std::make_shared<KdTrailPolygon>();
+	m_trailPolygon->ClearPoints();
+	m_trailPolygon->SetLength(100);
+	auto trailTex = KdAssets::Instance().m_textures.GetData("Asset/Textures/gradation_B.png");
+	m_trailPolygon->SetMaterial(trailTex);
+	m_trailPolygon->SetPattern(KdTrailPolygon::Trail_Pattern::eBillboard);
+}
+
+void TrailEffect::Update()
+{
+}
+
+void TrailEffect::DrawBright()
+{
+	KdShaderManager::Instance().m_StandardShader.DrawPolygon(*m_trailPolygon, Math::Matrix::Identity, { 1.0f,1.0f,1.0f,0.7f });
+}
---
+++ Src/Application/GameObject/Effect/TrailEffect/TrailEffect.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+class TrailEffect : public KdGameObject
+{
+public:
+	TrailEffect() = default;
+	~TrailEffect() override = default;
+
+	void Init() override;
+	void Update() override;
+	void DrawBright() override;
+
+private:
+
+	std::shared_ptr<KdTrailPolygon> m_trailPolygon; // 軌跡ポリゴン
+
+};
